80f17e9b6afaf3c8ca4de32e6b2f9bd8d4cfbf05
Willow O'Hara <william.ohara@mymail.champlain.edu>
2023-08-31 15:45:52 -0400

    Started on HelloTriangle

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -8,7 +8,29 @@
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
 
+float verticies[9] = {
+	 //x   //y   //z
+	-0.5, -0.5,  0.0,  //Left
+	 0.5, -0.5,  0.0,  //Right
+	 0.0,  0.5,  0.0   //Center
+};
+
+unsigned int vbo, vao;
+
 int main() {
+	//Define vbo as an array buffer, a special kind of buffer designed to supply vertex data
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	//Allocates space for vertex data and sends vertex data to the GPU using the array buffer. GL_STATIC_DRAW declares that this data is static rather than dynamic.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+	//Right now, this buffer is just filled with a random pile of bits that OpenGL doesn't know what to do with. The VAO will translate it into something it can work with.
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	//
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (const void*)0);
+	glEnableVertexAttribArray(0);
+
 	printf("Initializing...");
 	if (!glfwInit()) {
 		printf("GLFW failed to init!");
