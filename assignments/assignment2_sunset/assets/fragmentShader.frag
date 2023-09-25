92faebeae4576fd4697e13fdea4b6d881d061f02
Willow O'Hara <willow.ohara@mymail.champlain.edu>
2023-09-24 22:44:57 -0400

    Added sun; added more detail and more variables to ImGui menu

--- assignments/assignment2_sunset/assets/fragmentShader.frag
+++ assignments/assignment2_sunset/assets/fragmentShader.frag
@@ -1,15 +1,14 @@
 #version 450
 out vec4 FragColor;
 in vec2 UV;
-uniform vec3 _ColorTopRise;
-uniform vec3 _ColorBotRise;
-uniform vec3 _ColorTopSet;
-uniform vec3 _ColorBotSet;
-uniform vec3 _BuildingColorRise;
-uniform vec3 _BuildingColorSet;
+uniform vec3 _ColorTopRise, _ColorBotRise, _ColorTopSet, _ColorBotSet;
+uniform vec4 _BuildingColorRise, _BuildingColorSet;
+uniform vec4 _SunColorRise, _SunColorSet;
+uniform float _SunRadius, _SunOrbitRadius;
+uniform vec2 _SunOrbitPos;
 uniform float _Speed;
 uniform float _Time;
-uniform vec2 _Resoultion;
+uniform vec2 _Resolution;
 uniform float _BuildingHeight[10];
 
 float circleSDF(vec2 p, float r)
@@ -19,20 +18,33 @@ float circleSDF(vec2 p, float r)
 
 void main()
 {
-	//0 to 1
-	vec2 uv = UV/_Resoultion.xy;
-	//-1 to 1
-	uv= uv * 2.0 - 1.0;
-	//Correct for aspect ratio
-	uv.x*=_Resoultion.x / _Resoultion.y;
+	//Correct UV for resolution (for making round circles)
+	vec2 correctedUV = UV * 2.0 - 1.0;
+	correctedUV.x*=_Resolution.x / _Resolution.y;
 
+	//Sine and Cosine waves ocelating between 0 and 1 across time
 	float tSin = (sin(_Time/(1/_Speed)) + 0.5) / 2;
 	float tCos = (cos(_Time/(1/_Speed)) + 0.5) / 2;
 
-	vec3 buildingBrightness = mix(_BuildingColorRise, _BuildingColorSet, tSin);
-	vec3 topColor = mix(_ColorTopRise, _ColorTopSet, tSin);
-	vec3 botColor = mix(_ColorBotRise, _ColorBotSet, tSin);
-	vec3 color = mix(topColor,botColor,UV.y);
+	//Sky color
+	vec3 topColor = mix(_ColorTopRise, _ColorTopSet, tCos);
+	vec3 botColor = mix(_ColorBotRise, _ColorBotSet, tCos);
+	vec3 skyColor = mix(botColor,topColor,UV.y);
+	
+	//Sun color
+	vec4 sunRiseColor = _SunColorRise;
+	vec4 sunSetColor = _SunColorSet;
+	vec4 sunColor = mix(sunRiseColor, sunSetColor, tCos);
+	
+	//Sun position
+	vec2 sunPos = vec2((tSin - _SunOrbitPos.x) * -_SunOrbitRadius, (tCos - _SunOrbitPos.y) * -_SunOrbitRadius);
+	float sun = 1.0 - circleSDF(correctedUV - sunPos, _SunRadius - 1);
+	sun = smoothstep(-0.05,0.5,sun);
+	vec3 sunSky = mix(skyColor, sunColor.rgb, sun * sunColor.a);
+
+	//Buildings
 	float buildings = 1.0 - step(_BuildingHeight[int(floor(UV.x * 10))], UV.y);
-	FragColor = vec4(mix(color,buildingBrightness,buildings), 1);
+	vec4 buildingBrightness = mix(_BuildingColorRise, _BuildingColorSet, tCos);
+	FragColor = vec4(mix(sunSky,buildingBrightness.rgb,buildings * buildingBrightness.a), 1);
+
 }
--- assignments/assignment2_sunset/main.cpp
+++ assignments/assignment2_sunset/main.cpp
@@ -11,6 +11,7 @@
 #include <willowLib/shader.h>
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+void renderUI();
 
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
@@ -28,19 +29,40 @@ unsigned int indicies[6] = {
 	2, 3, 0
 };
 
-float horizonTopColorDay[3] = { 1.0f, 0.5f, 0.0f };
-float horizonBotColorDay[3] = { 0.5f, 0.5f, 1.0f };
-float horizonTopColorNight[3] = { 0.25f, 0.125f, 0.0f };
-float horizonBotColorNight[3] = { 0.125f, 0.125f, 0.25f };
+//Sky Colors (Ref constants exist to add base color on color pickers)
+float horizonTopColorDay[3] = { 0.5f, 0.5f, 1.0f };
+const float HTCD_REF[3] = { 0.5f, 0.5f, 1.0f };
+float horizonBotColorDay[3] = { 1.0f, 0.5f, 0.0f }; 
+const float HBCD_REF[3] = { 1.0f, 0.5f, 0.0f };
+float horizonTopColorNight[3] = { 0.125f, 0.125f, 0.25f }; 
+const float HTCN_REF[3] = { 0.125f, 0.125f, 0.25f };
+float horizonBotColorNight[3] = { 0.25f, 0.125f, 0.0f };
+const float HBCN_REF[3] = { 0.25f, 0.125f, 0.0f };
 
+//Sun Colors
+float sunColorDay[4] = { 1.0f, 1.0f, 0.0f, 0.8f };
+const float SCD_REF[4] = { 1.0f, 1.0f, 0.0f, 0.8f };
+float sunColorNight[4] = { 0.25f, 0.25f, 0.0f, 0.8f };
+const float SCN_REF[4] = { 0.25f, 0.25f, 0.0f, 0.8f };
+
+//Sun Path Details
+float sunOrbitPos[2] = { 0.25f, 0.0f };
+float sunRadius = 0.5f;
+float sunOrbitRadius = 2.0f;
 float sunSpeed = 1.0f;
 
-float buildingColorDay[3] = { 0.2, 0.2, 0.2 };
-float buildingColorNight[3] = { 0.1, 0.1, 0.1 };
-float buildings[10] = { 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
+//Building Details
+float buildingColorDay[4] = { 0.2f, 0.2f, 0.2f, 1.0f };
+const float BCN_REF[4] = { 0.1f, 0.1f, 0.1f, 1.0f };
+float buildingColorNight[4] = { 0.1f, 0.1f, 0.1f, 1.0f };
+const float BCD_REF[4] = { 0.2f, 0.2f, 0.2f, 1.0f };
+float buildings[10] = { 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1.0f};
+
 bool showImGUIDemoWindow = false;
 bool drawWireframe = false;
-float resolution[2] = { SCREEN_WIDTH, SCREEN_HEIGHT};
+int resolution[2] = { SCREEN_WIDTH, SCREEN_HEIGHT};
+
+
 
 int main() {
 	printf("Initializing...");
@@ -74,11 +96,14 @@ int main() {
 	unsigned int vao = willowLib::createVAO(vertices, 4, indicies, 6);
 	glBindVertexArray(vao);
 
-	srand(time(NULL));
+	srand((unsigned int)time(NULL));
 	for (int i = 0; i < sizeof(buildings)/sizeof(float); i++)
 	{
 		buildings[i] = ew::RandomRange(0, 1);
+		if (buildings[i] > 0.5)
+			buildings[i] = ew::RandomRange(0, 1);
 	}
+
 	
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
@@ -86,16 +111,29 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		//Set uniforms
+		shader.setFloat("_Speed", sunSpeed);
+		shader.setVec2("_Resolution", resolution[0], resolution[1]);
+		shader.setFloat("_Time", (float)glfwGetTime());
+		
+		//Sky Color
 		shader.setVec3("_ColorTopRise", horizonTopColorDay[0], horizonTopColorDay[1], horizonTopColorDay[2]);
 		shader.setVec3("_ColorBotRise", horizonBotColorDay[0], horizonBotColorDay[1], horizonBotColorDay[2]);
 		shader.setVec3("_ColorTopSet", horizonTopColorNight[0], horizonTopColorNight[1], horizonTopColorNight[2]);
 		shader.setVec3("_ColorBotSet", horizonBotColorNight[0], horizonBotColorNight[1], horizonBotColorNight[2]);
-		shader.setVec3("_BuildingColorRise", buildingColorDay[0], buildingColorDay[1], buildingColorDay[2]);
-		shader.setVec3("_BuildingColorSet", buildingColorNight[0], buildingColorNight[1], buildingColorNight[2]);
+		
+		//Sun Color
+		shader.setVec4("_SunColorRise", sunColorDay[0], sunColorDay[1], sunColorDay[2], sunColorDay[3]);
+		shader.setVec4("_SunColorSet", sunColorNight[0], sunColorNight[1], sunColorNight[2], sunColorNight[3]);
+		//Sun Orbit Details
+		shader.setVec2("_SunOrbitPos", sunOrbitPos[0], sunOrbitPos[1]);
+		shader.setFloat("_SunRadius", sunRadius);
+		shader.setFloat("_SunOrbitRadius", sunOrbitRadius);
+		
+		//Building details
+		shader.setVec4("_BuildingColorRise", buildingColorDay[0], buildingColorDay[1], buildingColorDay[2], buildingColorDay[3]);
+		shader.setVec4("_BuildingColorSet", buildingColorNight[0], buildingColorNight[1], buildingColorNight[2], buildingColorNight[3]);
 		shader.setFloatArray("_BuildingHeight", buildings, 10);
-		shader.setFloat("_Speed", sunSpeed);
-		shader.setVec2("_Resolution", resolution[0], resolution[1]);
-		shader.setFloat("_Time", glfwGetTime());
+		
 		if (drawWireframe)
 			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		else
@@ -103,56 +141,112 @@ int main() {
 
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);
 
-		//Render UI
-		{
-			ImGui_ImplGlfw_NewFrame();
-			ImGui_ImplOpenGL3_NewFrame();
-			ImGui::NewFrame();
-
-			ImGui::Begin("Settings");
-			ImGui::Checkbox("Show Demo Window", &showImGUIDemoWindow);
-			if (ImGui::CollapsingHeader("Sky Settings"))
-			{
-				ImGui::ColorEdit3("Upper Color (Day)", horizonTopColorDay);
-				ImGui::ColorEdit3("Lower Color (Day)", horizonBotColorDay);
-				ImGui::ColorEdit3("Upper Color (Night)", horizonTopColorNight);
-				ImGui::ColorEdit3("Lower Color (Night)", horizonBotColorNight);
-				ImGui::SliderFloat("Speed", &sunSpeed, 0.0f, 25.0f);
-			}
-			if (ImGui::CollapsingHeader("Building Settings"))
-			{
-				ImGui::ColorEdit3("Building Color (Day)", buildingColorDay);
-				ImGui::ColorEdit3("Building Color (Night)", buildingColorNight);
-				ImGui::PushID("Building Heights");
-				for (int i = 0; i < sizeof(buildings) / sizeof(float); i++)
-				{
-					if (i > 0) ImGui::SameLine();
-					ImGui::PushID(i);
-					ImGui::VSliderFloat("##v", ImVec2(18, 160), &buildings[i], 0.0f, 1.0f, "");
-					if (ImGui::IsItemActive() || ImGui::IsItemHovered())
-						ImGui::SetTooltip("%.3f", buildings[i]);
-					ImGui::PopID();
-				}
-				ImGui::PopID();
-			}
-			if (ImGui::CollapsingHeader("Specs"))
-			{
-				ImGui::Text("Resolution: (%i) x (%i)", (int)resolution[0], (int)resolution[1]);
-				ImGui::Text("Time: (%f)", glfwGetTime());
-			}
-
-			ImGui::End();
-			if (showImGUIDemoWindow) {
-				ImGui::ShowDemoWindow(&showImGUIDemoWindow);
-			}
-
-			ImGui::Render();
-			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-		}
+		renderUI();
 
 		glfwSwapBuffers(window);
 	}
 	printf("Shutting down...");
+}
+
+int colorPickerChoice = 0;
+
+void renderUI()
+{
+	ImGui_ImplGlfw_NewFrame();
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui::NewFrame();
+	float w = (ImGui::GetContentRegionAvail().x - ImGui::GetStyle().ItemSpacing.y) * 0.50f;
+	ImGuiColorEditFlags flags = ImGuiColorEditFlags_AlphaPreview | ImGuiColorEditFlags_AlphaBar | ImGuiColorEditFlags_DisplayRGB;
+
+	ImGui::Begin("Settings");
+	//ImGui::Checkbox("Show Demo Window", &showImGUIDemoWindow);
+	if (ImGui::CollapsingHeader("Sky Settings"))
+	{
+		ImGui::SliderFloat("Speed", &sunSpeed, 0.0f, 25.0f);
+		ImGui::Text("Color to Edit");
+		ImGui::Combo(" ", &colorPickerChoice, "Day Color\0Night Color\0Sun Settings\0All Sky Settings");
+		switch (colorPickerChoice)
+		{
+		case 0:
+			flags |= ImGuiColorEditFlags_NoAlpha;
+			ImGui::SetNextItemWidth(w);
+			ImGui::ColorPicker4("Upper Color (Day)", horizonTopColorDay, flags, HTCD_REF);
+			ImGui::SameLine();
+			ImGui::SetNextItemWidth(w);
+			ImGui::ColorPicker4("Lower Color (Day)", horizonBotColorDay, flags, HBCD_REF);
+			break;
+		case 1:
+			flags |= ImGuiColorEditFlags_NoAlpha;
+			ImGui::SetNextItemWidth(w);
+			ImGui::ColorPicker4("Upper Color (Night)", horizonTopColorNight, flags, HTCN_REF);
+			ImGui::SameLine();
+			ImGui::SetNextItemWidth(w);
+			ImGui::ColorPicker4("Lower Color (Night)", horizonBotColorNight, flags, HTCD_REF);
+			break;
+		case 2:
+			flags |= ImGuiColorEditFlags_AlphaBar;
+			ImGui::SetNextItemWidth(w);
+			ImGui::ColorPicker4("Sun Color (Day)", sunColorDay, flags, SCD_REF);
+			ImGui::SameLine();
+			ImGui::SetNextItemWidth(w);
+			ImGui::ColorPicker4("Sun Color (Night)", sunColorNight, flags, SCN_REF);
+			ImGui::SliderFloat("Sun Radius", &sunRadius, 0.0f, 5.0f, "%.3f");
+			ImGui::SliderFloat2("Sun Path Center", sunOrbitPos, -2, 2);
+			break;
+		case 3:
+		default:
+			flags |= ImGuiColorEditFlags_NoAlpha;
+			ImGui::SetNextItemWidth(w);
+			ImGui::ColorPicker4("Upper Color (Day)", horizonTopColorDay, flags, HTCD_REF);
+			ImGui::SameLine();
+			ImGui::SetNextItemWidth(w);
+			ImGui::ColorPicker4("Lower Color (Day)", horizonBotColorDay, flags, HBCD_REF);
+			ImGui::SetNextItemWidth(w);
+			ImGui::ColorPicker4("Upper Color (Night)", horizonTopColorNight, flags, HTCN_REF);
+			ImGui::SameLine();
+			ImGui::SetNextItemWidth(w);
+			ImGui::ColorPicker4("Lower Color (Night)", horizonBotColorNight, flags, HBCN_REF);
+			flags |= ImGuiColorEditFlags_AlphaBar; //This doesn't work for some reason. Neither does passing "(flags | ImGuiColorEditFlags_AlphaBar)" into the flags argument. Not sure why.
+			ImGui::SetNextItemWidth(w);
+			ImGui::ColorPicker4("Sun Color (Day)", sunColorDay, flags, SCD_REF);
+			ImGui::SameLine();
+			ImGui::SetNextItemWidth(w);
+			ImGui::ColorPicker4("Sun Color (Night)", sunColorNight, flags, SCN_REF);
+			ImGui::SliderFloat("Sun Radius", &sunRadius, 0.0f, 5.0f, "%.3f");
+			ImGui::SliderFloat2("Sun Path Center", sunOrbitPos, -2, 2);
+			break;
+		}
+	}
+	if (ImGui::CollapsingHeader("Building Settings"))
+	{
+		flags |= ImGuiColorEditFlags_AlphaBar;
+		ImGui::SetNextItemWidth(w);
+		ImGui::ColorPicker4("Building Color (Day)", buildingColorDay, flags, buildingColorDay);
+		ImGui::SameLine();
+		ImGui::SetNextItemWidth(w);
+		ImGui::ColorPicker4("Building Color (Night)", buildingColorNight, flags, buildingColorNight);
+		ImGui::Text("Building Heights");
+		for (int i = 0; i < sizeof(buildings) / sizeof(float); i++)
+		{
+			if (i > 0) ImGui::SameLine();
+			ImGui::PushID(i); //Needs id here or else it makes all the sliders the same value
+			ImGui::VSliderFloat("##v", ImVec2(40, 160), &buildings[i], 0.0f, 1.0f, "");
+			ImGui::PopID();
+		}
+	}
+	if (ImGui::CollapsingHeader("Specs"))
+	{
+		ImGui::Text("Resolution: (%i) x (%i)", (int)resolution[0], (int)resolution[1]);
+		ImGui::Text("Time: (%f)", glfwGetTime());
+	}
+
+	ImGui::End();
+	if (showImGUIDemoWindow) {
+		ImGui::ShowDemoWindow(&showImGUIDemoWindow);
+	}
+
+	ImGui::Render();
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height)
