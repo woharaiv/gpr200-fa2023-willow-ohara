78963d16c988282cb218709884eba0c29099ccee
willow.ohara <willow.ohara@mymail.champlain.edu>
2023-10-19 15:48:38 -0400

    Finished Assignment 5

--- assignments/assignment5_camera/main.cpp
+++ assignments/assignment5_camera/main.cpp
@@ -11,10 +11,11 @@
 #include <ew/shader.h>
 #include <ew/procGen.h>
 #include <ew/transform.h>
+#include <ew/ewMath/ewMath.h>
 #include <willowLib/camera.h>
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
-void moveCamera(GLFWwindow* window, willowLib::Camera* camera, willowLib::CameraControls* controls);
+void moveCamera(GLFWwindow* window, willowLib::Camera* camera, willowLib::CameraControls* controls, float deltaTime);
 
 //Projection will account for aspect ratio!
 const int SCREEN_WIDTH = 1080;
@@ -73,10 +74,16 @@ int main() {
 		cubeTransforms[i].position.y = i / (NUM_CUBES / 2) - 0.5;
 	}
 	cam.position = {0, 0, 5};
-
+	float prevTime = (float)glfwGetTime();
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
-		moveCamera(window, &cam, &cameraControls);
+
+		//Calculate deltaTime
+		float time = (float)glfwGetTime();
+		float deltaTime = time - prevTime;
+		prevTime = time;
+
+		moveCamera(window, &cam, &cameraControls, deltaTime);
 		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
 		//Clear both color buffer AND depth buffer
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -102,16 +109,18 @@ int main() {
 			ImGui::NewFrame();
 
 			ImGui::Begin("Settings");
-			ImGui::Text("Cubes");
-			for (size_t i = 0; i < NUM_CUBES; i++)
+			if (ImGui::CollapsingHeader("Cubes"))
 			{
-				ImGui::PushID(i);
-				if (ImGui::CollapsingHeader("Transform")) {
-					ImGui::DragFloat3("Position", &cubeTransforms[i].position.x, 0.05f);
-					ImGui::DragFloat3("Rotation", &cubeTransforms[i].rotation.x, 1.0f);
-					ImGui::DragFloat3("Scale", &cubeTransforms[i].scale.x, 0.05f);
+				for (size_t i = 0; i < NUM_CUBES; i++)
+				{
+					ImGui::PushID(i);
+					if (ImGui::CollapsingHeader("Transform")) {
+						ImGui::DragFloat3("Position", &cubeTransforms[i].position.x, 0.05f);
+						ImGui::DragFloat3("Rotation", &cubeTransforms[i].rotation.x, 1.0f);
+						ImGui::DragFloat3("Scale", &cubeTransforms[i].scale.x, 0.05f);
+					}
+					ImGui::PopID();
 				}
-				ImGui::PopID();
 			}
 			if (ImGui::CollapsingHeader("Camera"))
 			{
@@ -121,8 +130,26 @@ int main() {
 				if (cam.orthogrpahic)
 					ImGui::DragFloat("Orhtographic Frustum Height", &cam.orthoSize);
 				else
+				{
 					ImGui::DragFloat("FOV", &cam.fov, 0.1f);
+					ImGui::DragFloat("Near Plane", &cam.nearPlane, 0.1f);
+					ImGui::DragFloat("Far Plane", &cam.farPlane, 0.1f);
+				}
 			}
+			if (ImGui::CollapsingHeader("Camera Controller"))
+			{
+				ImGui::Text("Yaw: %.0f", cameraControls.yaw);
+				ImGui::Text("Pitch : %.0f", cameraControls.pitch);
+				ImGui::DragFloat("Move Speed", &cameraControls.moveSpeed, 0.1f);
+				ImGui::DragFloat("Sensitivity", &cameraControls.mouseSensitivity);
+				ImGui::Checkbox("Invert Y", &cameraControls.invertY);
+			}
+			if (ImGui::Button("Reset"))
+			{
+				cam.ResetCam();
+				cameraControls.ResetCamControls();
+			}
+			
 			ImGui::End();
 			
 			ImGui::Render();
@@ -134,36 +161,66 @@ int main() {
 	printf("Shutting down...");
 }
 
-void moveCamera(GLFWwindow* window, willowLib::Camera* camera, willowLib::CameraControls* controls)
+void moveCamera(GLFWwindow* window, willowLib::Camera* camera, willowLib::CameraControls* controls, float deltaTime)
 {
 	//Camera only moves on right click; release mouse and return early if not right clicking.
 	if (!glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT))
 	{
 		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
 		controls->firstMouse = true;
-		return;
 	}
-	//Hide cursor, but it keeps moving
-	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
-	//Get this frame's mouse position
-	double mouseX, mouseY;
-	glfwGetCursorPos(window, &mouseX, &mouseY);
-
-	if (controls->firstMouse)
+	else
 	{
-		controls->firstMouse = false;
+		//Hide cursor, but it keeps moving
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+		//Get this frame's mouse position
+		double mouseX, mouseY;
+		glfwGetCursorPos(window, &mouseX, &mouseY);
+
+		if (controls->firstMouse)
+		{
+			controls->firstMouse = false;
+			controls->prevMouseX = mouseX;
+			controls->prevMouseY = mouseY;
+		}
+
+		//Get position delta
+		double mouseXDelta = mouseX - controls->prevMouseX;
+		double mouseYDelta = mouseY - controls->prevMouseY;
+		//Add to yaw and pitch
+		if (!cameraControls.invertY)
+			mouseYDelta *= -1;
+		controls->yaw += mouseXDelta * controls->mouseSensitivity * deltaTime;
+		controls->pitch += mouseYDelta * controls->mouseSensitivity * deltaTime;
+		//Clamp pitch between -89 and 89
+		if (controls->pitch > 89)
+			controls->pitch = 89;
+		else if (controls->pitch < -89)
+			controls->pitch = -89;
+		//Remember previous mouse position
 		controls->prevMouseX = mouseX;
 		controls->prevMouseY = mouseY;
 	}
-
-	//Get position delta
-	//Add to yaw and pitch
-	//Clamp pitch between -89 and 89
-
-	//Remember previous mouse position
-	controls->prevMouseX = mouseX;
-	controls->prevMouseY = mouseY;
+	double yawRad = controls->yaw * ew::DEG2RAD;
+	double pitchRad = controls->pitch * ew::DEG2RAD;
+	ew::Vec3 forward = ew::Vec3(sin(yawRad) * cos(pitchRad), sin(pitchRad), -cos(yawRad) * cos(pitchRad));
+	ew::Vec3 right = ew::Normalize(ew::Cross(forward, ew::Vec3(0, 1, 0)));
+	ew::Vec3 up = ew::Normalize(ew::Cross(right, forward));
+	if (glfwGetKey(window, GLFW_KEY_W))
+		camera->position += forward * controls->moveSpeed * deltaTime;
+	if (glfwGetKey(window, GLFW_KEY_S))
+		camera->position += -1 * forward * controls->moveSpeed * deltaTime;
+	if (glfwGetKey(window, GLFW_KEY_A))
+		camera->position += -1 * right * controls->moveSpeed * deltaTime;
+	if (glfwGetKey(window, GLFW_KEY_D))
+		camera->position += right * controls->moveSpeed * deltaTime;
+	if (glfwGetKey(window, GLFW_KEY_Q))
+		camera->position += -1* up * controls->moveSpeed * deltaTime;
+	if (glfwGetKey(window, GLFW_KEY_E))
+		camera->position += up * controls->moveSpeed * deltaTime;
+	
+	camera->target = camera->position + forward;
 }
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height)
--- core/willowLib/camera.h
+++ core/willowLib/camera.h
@@ -4,22 +4,48 @@
 
 namespace willowLib {
 	struct Camera {
-		ew::Vec3 position = (0, 0, 5); //Camera "eye" position
-		ew::Vec3 target = (0, 0, 0); //Position to look at
+		ew::Vec3 position = ew::Vec3(0, 0, 5); //Camera "eye" position
+		ew::Vec3 target = ew::Vec3(0, 0, 0); //Position to look at
+		ew::Vec3 f = ew::Vec3(0, 0, -1);
 		float fov = 60; //Field of view in degrees
 		float aspectRatio = 4/3; //Screen width / screen height
 		float nearPlane = 0.1; //Near plane distance (+Z)
 		float farPlane = 100; //Far plane distance (+Z)
-		bool orthogrpahic = true; //Orhtographic or perspective?
+		bool orthogrpahic = false; //Orhtographic or perspective?
 		float orthoSize = 6; //Height of orthogrpahic frustum
 		ew::Mat4 ViewMatrix(); //World -> view
 		ew::Mat4 ProjectionMatrix(); //View -> clip
+
+		const ew::Vec3 START_POS = position;
+		const ew::Vec3 START_TARGET = target;
+		const float START_FOV = fov;
+		const float START_NEAR_PLANE = nearPlane;
+		const float START_FAR_PLANE = farPlane;
+		const float START_ORTHO_SIZE = orthoSize;
+		void ResetCam()
+		{
+			position = START_POS;
+			target = START_TARGET;
+			fov = START_FOV;
+			nearPlane = START_NEAR_PLANE;
+			farPlane = START_FAR_PLANE;
+			orthoSize = START_ORTHO_SIZE;
+		}
 	};
 	struct CameraControls {
 		double prevMouseX, prevMouseY; //Mouse position of previous frame
 		float yaw = 0, pitch = 0; //In Degrees
-		float mouseSensitivity = 0.1f;
+		float mouseSensitivity = 10.0f;
 		bool firstMouse = true; //Flag to help store initial mouse position
 		float moveSpeed = 5.0f;
+		bool invertY = false;
+		
+		const float START_YAW = yaw;
+		const float START_PITCH = pitch;
+		void ResetCamControls()
+		{
+			yaw = START_YAW;
+			pitch = START_PITCH;
+		}
 	};
 }
